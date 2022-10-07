c39546ff2108641f48d8a90be43714e272ee27d2
Popivzanin <81978533+Popivzanin@users.noreply.github.com>
2022-10-07 23:53:52 +0300

    Update task3.py

--- task/task3.py
+++ task/task3.py
@@ -1,94 +1,73 @@
-import csv
-import sys
 from io import StringIO
+import csv
 
+def read(csvString):
+  f = StringIO(csvString)
+  reader = csv.reader(f, delimiter=',')
+  graph = []
+  for row in reader:
+     graph.append(row)
+  for x in graph:
+    for y in x:
+      y = int(y)
+  return graph
 
-'''
-Ввод:
-from,to
-1,2
-1,3
-2,4
-'''
+def rel1(graph):
+  arr = []
+  for x in graph:
+    if x[0] not in arr:
+      arr.append(str(x[0]))
+  return arr
 
+def rel2(graph):
+  arr = []
+  for x in graph:
+    if x[1] not in arr:
+      arr.append(str(x[1]))
+  return arr
 
-# Функция для считывания данных из входного потока
-def read_csv():
-    # Считываем все линии входного потока до закрывающего ^D (Ctrl + D)
-    csv_content = sys.stdin.readlines()
-    # Десериализируем входные данные
-    reader = csv.reader(csv_content)
-    # Убираем строку хедеров
-    next(reader)
-    # Преобразуем перечислимый тип в список
-    data = list(reader)
-    # Возвращаем полученный список связей
-    return data
+def rel3(graph):
+  f = graph
+  g = graph
+  arr = []
+  for i in range(len(f)):
+    for j in range(len(g)):
+      if i != j and f[i][0] not in arr and f[i][1] == g[j][0]:
+        arr.append(str(f[i][0]))
+  return arr
 
+def rel4(graph):
+  f = graph
+  g = graph
+  arr = []
+  for i in range(len(f)):
+    for j in range(len(g)):
+      if i != j and f[i][1] not in arr and f[i][0] == g[j][1]:
+        arr.append(str(f[i][1]))
+  return arr
 
-# Функция для классификации отношений между элементами
-def classify(cons: list):
-    # Создаем пустые списки для каждого типа отношений
-    t1 = list()
-    t2 = list()
-    t3 = list()
-    t4 = list()
-    t5 = list()
+def rel5(graph):
+  f = graph
+  g = graph
+  arr = []
+  for i in range(len(f)):
+    for j in range(len(g)):
+      if i != j and f[i][1] not in arr and f[i][0] == g[j][0]:
+        arr.append(str(f[i][1]))
+  return arr
 
-    # Идем в цикле по всем данным связям. Пусть con - первая связь
-    for con in cons:
-        # Добавляем первую связь в список 1го типа отношений
-        t1.append(con)
-        # Добавляем первую связь, но в обратном порядке, в список 2го типа отношений
-        t2.append(con[::-1])
+def task(csvString):
+    graph = read(csvString)
+    # Прямое управление
+    arr1 = rel1(graph)
+    # Прямое подчинение
+    arr2 = rel2(graph)
+    # Косвенное управление
+    arr3 = rel3(graph)
+    # Косвенное подчинение
+    arr4 = rel4(graph)
+    # Соподчинение
+    arr5 = rel5(graph)
 
-        # Идем в цикле по всем данным связям, кроме текущей. Пусть other_con - вторая связь
-        for other_con in [c for c in cons if c != con]:
-            # Если совпадает конец первой и начало второй связей
-            if con[1] == other_con[0]:
-                # Добавляем связь из начала первой связи в конец второй связи в список 3го типа отношений
-                t3.append([con[0], other_con[1]])
-                # Добавляем связь из конца первой связи в начала второй связи в список 4го типа отношений
-                t4.append([other_con[1], con[0]])
-            # Если совпадают начала первой и второй связей
-            elif con[0] == other_con[0]:
-                # Добавляем связь из конца первой в конец второй связи в список 5го типа отношений
-                t5.append([con[1], other_con[1]])
-
-    # Возвращаем списки всех типов отношений
-    return t1, t2, t3, t4, t5
-
-
-# Функция, заданная по условию в Canvas
-def task(csv_str: str) -> None:
-    csv_io = StringIO(csv_str)
-    reader = csv.reader(csv_io)
-    data = list(reader)
-    type_1, type_2, type_3, type_4, type_5 = classify(data)
-    print(f"""\
-Тип 1: {type_1}
-Тип 2: {type_2}
-Тип 3: {type_3}
-Тип 4: {type_4}
-Тип 5: {type_5}\
-""")
-
-
-# Функция, которая была написана на паре
-def main():
-    # Считываем входные данные, занося их в список связей
-    connections = read_csv()
-    # Классифицируем по списку связей, получая 5 списков - для каждого типа отношений
-    type_1, type_2, type_3, type_4, type_5 = classify(connections)
-    # Выводим содержимое списков
-    print(f"""\
-    Тип 1: {type_1}
-    Тип 2: {type_2}
-    Тип 3: {type_3}
-    Тип 4: {type_4}
-    Тип 5: {type_5}\
-    """)
-
-
-if __name__ == "__main__":
-    task("1,2\n1,3\n2,4")
+    # Результаты
+    return [arr1, arr2, arr3, arr4, arr5]
